69bf4233e73372cf27f4281b687e12d2cc91a739
Pof Magicfingers <pof@pof.pm>
2020-12-28 20:25:34 +0100

    wip

--- podcloud/podcloud.php
+++ podcloud/podcloud.php
@@ -22,43 +22,57 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       podcloud
  * Domain Path:       /languages
-*/
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
-
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PODCLOUD_VERSION', '1.0.0' );
 
-function podcloud_helper( $html, $url, $attr, $post_ID ) {
-        global $has_podcloud_helper;
-        if ( false !== strpos( $url, 'podcloud.fr' ) ) {
-                if($has_podcloud_helper !== true) {
-                        $script = '<script src="https://podcloud.fr/player-embed/helper.js"></script>';
-                        $has_podcloud_helper = true;
-                        return $script . $html;
-                }
-        }
-
-        return $html;
+// If this file is called directly, abort.
+if ( ! defined( "WPINC" ) ) {
+    die;
 }
+
+define( "PODCLOUD_VERSION", "1.0.0" );
+define( "PODCLOUD_HELPER_SCRIPT", "https://podcloud.fr/player-embed/helper.js" );
+
+function flag_podcloud_helper_need( $html, $url, $attr, $post_ID ) {
+    global $need_podcloud_helper;
+
+    if ( false !== strpos( $url, "podcloud.fr" ) ) {
+        $need_podcloud_helper = true;
+    }
+
+    return $html;
+}
+add_filter( "embed_oembed_html", "flag_podcloud_helper_need", 99, 4 );
+
+function enqueue_podcloud_helper() {
+    global $need_podcloud_helper;
+
+    if($need_podcloud_helper) {
+        wp_enqueue_script(
+            "podcloud_helper",
+            PODCLOUD_HELPER_SCRIPT,
+            array(),
+            PODCLOUD_VERSION
+        );
+    }
+}
+
+global $need_podcloud_helper;
+
+$need_podcloud_helper = false;
+
+add_action( "wp_enqueue_scripts", "enqueue_podcloud_helper" );
+add_action( "admin_enqueue_scripts", "enqueue_podcloud_helper" );
+add_action( "enqueue_embed_scripts", "enqueue_podcloud_helper" );
 
 $supported = [
-        "http://podcloud.fr/podcast/*",
-        "https://podcloud.fr/podcast/*",
-        "http://*.lepodcast.fr/*",
-        "https://*.lepodcast.fr/*",
-        "http://pdca.st/*",
-        "https://pdca.st/*",
+    "http://podcloud.fr/podcast/*",
+    "https://podcloud.fr/podcast/*",
+    "http://*.lepodcast.fr/*",
+    "https://*.lepodcast.fr/*",
+    "http://pdca.st/*",
+    "https://pdca.st/*",
 ];
 
 foreach($supported as $format) {
     wp_oembed_add_provider( $format, "https://podcloud.fr/embed" );
 }
-
-add_filter( 'embed_oembed_html', 'podcloud_helper', 99, 4 );
